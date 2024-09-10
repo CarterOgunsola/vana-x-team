a64907c592a4de6a034a67ace6719ac458cf5623
Carter Ogunsola <carterogunsola@gmail.com>
2024-09-10 07:49:15 +0000

    Chore: Updated animations and prep for client v1.0 live update

--- src/js/animations/dlp/swiper.js
+++ src/js/animations/dlp/swiper.js
@@ -30,45 +30,64 @@ const updateActiveNavEl = () => {
 const controlInitialSliderUI = () => {
   const sliderPaginationCtrl = document.querySelector(".dlp-page-control");
   const sliderPageStatus = document.querySelector(".status");
+  const paginationBullets = document.querySelectorAll(
+    ".swiper-pagination-bullet"
+  );
 
   const ctrlTL = gsap.timeline();
 
   ctrlTL
     .to(sliderPaginationCtrl, {
       opacity: 1,
-      y: "0%",
-      duration: 0.4,
-      ease: "power2.in",
+      y: 0,
+      duration: 1,
+      ease: "expo.out",
     })
-    .to(sliderPageStatus, {
-      opacity: 1,
-      duration: 0.4,
-      ease: "power2.in",
-    });
+    .to(
+      paginationBullets,
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        ease: "expo.out",
+        stagger: 0.05, // Small stagger to delay each bullet slightly
+      },
+      "-=0.8"
+    )
+    .to(
+      sliderPageStatus,
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 0.8,
+        ease: "expo.out",
+      },
+      "-=0.8"
+    );
 };
 
 // init Swiper:
 export const swiperInit = () => {
   return new Swiper(".swiper", {
     spaceBetween: 30,
-    effect: "fade",
+    effect: "slide",
 
     autoplay: true,
 
     modules: [Navigation, Pagination],
 
-    speed: 400,
+    speed: 700,
 
-    // Optional parameters
     direction: "horizontal",
     // loop: true,
 
-    // If we need pagination
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
 
-    // Navigation arrows
+    allowTouchMove: false,
+
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
